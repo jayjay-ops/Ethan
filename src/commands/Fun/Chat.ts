0761a897a4ca093045f5a12698141d8e7b9a09b5
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 00:24:59 +0100

    Update Chat.ts

--- src/commands/Fun/Chat.ts
+++ src/commands/Fun/Chat.ts
@@ -9,7 +9,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'ᴄʜᴀᴛ',
             description: 'Chat with the Bot in group',
-            aliases: ['bot'],
+            aliases: ['bot','chat'],
             category: 'fun',
             usage: `${client.config.prefix}bot (text)`,
             baseXp: 30
