104691b1d4f6ba37f04ef7e163fc87e15676fac4
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 17:40:37 +0100

    Update Mods.ts

--- src/commands/General/Mods.ts
+++ src/commands/General/Mods.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'mods',
+            command: 'ᴍᴏᴅs',
             description: "Displays the Moderators' contact info",
             category: 'general',
             usage: `${client.config.prefix}mods`,
