6feff0db516b30d3ed757a077103133554c64f03
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 17:41:52 +0100

    Update Rank.ts

--- src/commands/General/Rank.ts
+++ src/commands/General/Rank.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from "../../typings";
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
 		super(client, handler, {
-			command: "rank",
+			command: "ʀᴀɴᴋ",
 			description: "Displays User's Stats",
 			category: "general",
 			usage: `${client.config.prefix}rank [tag/quote]`,
