2e18442f80bf619573459af681c8af75c0302163
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 17:42:29 +0100

    Update Xp.ts

--- src/commands/General/Xp.ts
+++ src/commands/General/Xp.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'xp',
+            command: 'xᴘ',
             description: "Displays User's Xp 🌟",
             category: 'general',
             usage: `${client.config.prefix}xp (@tag)`,
