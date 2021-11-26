4f8f53500cfa9795eac4543e04c7d76fad79c6f3
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 18:54:07 +0100

    Update Leave.ts

--- src/commands/Dev/Leave.ts
+++ src/commands/Dev/Leave.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'leave',
+            command: 'ʟᴇᴀᴠᴇ',
             description: 'Bot Leaves the group',
             category: 'dev',
             dm: true,
