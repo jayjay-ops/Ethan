ff19ce5cdbb3d9bb0f9efebcd856cca2b3259601
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 18:51:30 +0100

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -7,7 +7,7 @@ import {  IParsedArgs, ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'broadcast',
+            command: 'ʙʀᴏᴀᴅᴄᴀsᴛ',
             description: 'Tags all users in group chat',
             aliases: ['BC', 'announcement','bc'],
             category: 'dev',
