78446bbc8928efbf13bf17484273912ba5a52650
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 18:47:21 +0100

    Update JoinGc.ts

--- src/commands/Config/JoinGc.ts
+++ src/commands/Config/JoinGc.ts
@@ -1,26 +1,3 @@
-/* import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import { ISimplifiedMessage } from '../../typings'
-
-export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: `requestjoin`,
-            description: 'users request bot to join group',
-            category: 'config',
-            dm: true,
-            usage: `${client.config.prefix}requestjoin,
-            baseXp: 0
-        })
-    }
-
-    run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void (await M.reply(`📮 I will be more than happy to join!\nBut you must ask my Developer first\n\n👨🏽‍💻 ➪ wa.me/+2349051064375`))
-    }
-}
-*/
-
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
@@ -29,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'requestjoin',
+            command: 'ʀᴇǫᴜᴇsᴛᴊᴏɪɴ',
             description: 'Generally used to check if bot is Up',
             category: 'config',
             usage: `${client.config.prefix}requestjoin`,
@@ -38,6 +15,6 @@ export default class Command extends BaseCommand {
     }
 
     run = async (M: ISimplifiedMessage): Promise<void> => {
-        return void (await M.reply(`📮 ${M.sender.username}, I will be more than happy to join!\nBut you must ask my Developer first\n\n👨🏽‍💻 ➪ wa.me/+2349051064375`))
+        return void (await M.reply(`*📮 ${M.sender.username},\nI will be more than happy to join your group!\nBut you must ask my Developer first\n\n👨🏾‍💻 ➻ wa.me/+2349051064375*`))
     }
 }
