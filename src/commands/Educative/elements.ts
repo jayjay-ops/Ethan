7e5f1eb58a463ac0931ce3b116daf9336e6a0f3a
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 19:15:42 +0100

    Update elements.ts

--- src/commands/Educative/elements.ts
+++ src/commands/Educative/elements.ts
@@ -6,7 +6,7 @@ import axios from 'axios'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'elements',
+            command: 'ᴇʟᴇᴍᴇɴᴛs',
             description: 'get the info of the chemical element',
             aliases: ['element'],
             category: 'educative',
