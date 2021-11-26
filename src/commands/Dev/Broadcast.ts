1ded6c9faf6eba6348adb86aceeb5b231c30c3e1
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 23:43:54 +0100

    Update Broadcast.ts

--- src/commands/Dev/Broadcast.ts
+++ src/commands/Dev/Broadcast.ts
@@ -9,7 +9,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'ʙʀᴏᴀᴅᴄᴀsᴛ',
             description: 'Tags all users in group chat',
-            aliases: ['BC', 'announcement','bc'],
+            aliases: ['broadcast', 'announcement','bc'],
             category: 'dev',
             usage: `${client.config.prefix}broadcast`,
             modsOnly: true,
@@ -22,7 +22,7 @@ run = async (M: ISimplifiedMessage,  { joined }: IParsedArgs): Promise<void> => 
         const term = joined.trim();
         const chats:any= this.client.chats.all().filter(v => !v.read_only && !v.archive).map(v => v.jid).map(jids => jids.includes("g.us")? jids : null).filter(v=>v);
         for(let i =0;i<chats.length;i++){
-        const text = `*📮 🄱🅁🄾🄰🄳🄲🄰🅂🅃* \n\n *_✤━【 ${term} 】━✤_*\n\n *『 📬  𝒇𝒓𝒐𝒎 𝒐𝒘𝒏𝒆𝒓 🕵️ 』*`
+        const text = `*📮 🄱🅁🄾🄰🄳🄲🄰🅂🅃* \n\n${term}\n\n *『 📬  𝒇𝒓𝒐𝒎 𝒐𝒘𝒏𝒆𝒓 🕵️ 』*`
         this.client.sendMessage(chats[i], text,MessageType.text,{contextInfo : {mentionedJid : M.groupMetadata?.participants.map((user) => user.jid) }})
         }
     }
