0a566899339e1f4b31a6f8f332e6317285657ba8
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 00:53:01 +0100

    Update Help.ts

--- src/commands/General/Help.ts
+++ src/commands/General/Help.ts
@@ -38,11 +38,11 @@ export default class Command extends BaseCommand {
             let text = `\n漫~*'¨¯¨'*舞~舞*'¨¯¨·~漫\n\n ❥︎ 𝐻𝑒𝑦 𝐹𝑟𝑖𝑒𝑛𝑑, 𝐸𝑡𝒉𝑎𝑛-𝐵𝑜𝑡 𝑎𝑡 𝑦𝑜𝑢𝑟 𝑠𝑒𝑟𝑣𝑖𝑐𝑒\n\n漫~*'¨¯¨'*舞~舞*'¨¯¨·~漫\n\n\n『 𝐬𝒖𝒑𝒑𝒐𝒓𝒕 𝒖𝒔 𝒃𝒚 𝒇𝒐𝒍𝒍𝒐𝒘𝒊𝒏𝒈 𝒖𝒔 𝒐𝒏 𝒊𝒏𝒔𝒕𝒂𝒈𝒓𝒂𝒎 ➬ https://www.instagram.com/jayjay_ops 』\n\n\n\n`
             const keys = Object.keys(categories).sort((a, b) => a.localeCompare(b))
             for (const key of keys)
-                text += `${this.emojis[keys.indexOf(key)]} \`\`\`⊰⊱┈─╌❊ ${this.client.util.capitalize(key)} ❊╌─┈⊰⊱\`\`\`\n\n➣ *${categories[
+                text += `${this.emojis[keys.indexOf(key)]} \`\`\`⊰⊱┈─╌❊ ${this.client.util.capitalize(key)} ❊╌─┈⊰⊱\`\`\`\n\n➣ ${categories[
                     key
                 ]
                     .map((command) => command.config?.command)
-                    .join('\n ')}*\n\n\n`
+                    .join('\n ')}\n\n\n`
             return void this.client.sendMessage(M.from, { url: becky }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
             caption: `${text}  
