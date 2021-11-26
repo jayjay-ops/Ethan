2974d28737d436c309b61504bbb995e1e2e7445a
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 00:31:07 +0100

    Update Reactions.ts

--- src/commands/Fun/Reactions.ts
+++ src/commands/Fun/Reactions.ts
@@ -11,6 +11,7 @@ export default class Command extends BaseCommand {
             description: `Let's React`,
             aliases: [
                 'react',
+                'reaction'
                 'cry',
                 'kiss',
                 'bully',
