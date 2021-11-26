93920fc138a84ab43fe4659b08eea5707515cef0
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 18:30:03 +0100

    Update Enable.ts

--- src/commands/Config/Enable.ts
+++ src/commands/Config/Enable.ts
@@ -6,7 +6,7 @@ import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'enable',
+            command: 'ᴇɴᴀʙʟᴇ',
             description: 'Enables the given command globally',
             category: 'config',
             dm: true,
