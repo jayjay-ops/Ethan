a34617615a8767ed89316faac3e11818dc5a736f
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 18:16:51 +0100

    Update Guide.ts

--- src/commands/Bots/Guide.ts
+++ src/commands/Bots/Guide.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'guide',
+            command: 'ɢᴜɪᴅᴇ',
             description: 'Lists All Kaoi Guides',
             category: 'bots',
             usage: `${client.config.prefix}guide`,
