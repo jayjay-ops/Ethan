197086371bfd956ed1b22491fc8c934dec0666a0
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 20:25:09 +0100

    Update Activate.ts

--- src/commands/Moderation/Activate.ts
+++ src/commands/Moderation/Activate.ts
@@ -7,7 +7,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             adminOnly: true,
-            command: 'activate',
+            command: 'ᴀᴄᴛɪᴠᴀᴛᴇ',
             aliases: ['act'],
             description: 'activate certain features on group-chats',
             category: 'moderation',
