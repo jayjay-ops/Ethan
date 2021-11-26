b59c272d4c46d36bc54f87d52aa8f911289aa7f5
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 20:37:51 +0100

    Update Hentai.ts

--- src/commands/Porno/Hentai.ts
+++ src/commands/Porno/Hentai.ts
@@ -10,7 +10,7 @@ import { MessageType } from '@adiwajshing/baileys'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'hentai',
+            command: 'ʜᴇɴᴛᴀɪ',
             description: 'well....',
 
             aliases: ['henati'],
