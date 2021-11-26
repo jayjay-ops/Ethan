fab72d5f40791ddeaf53ecdb73c0ec16883585fc
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-26 17:38:13 +0100

    Update Hi.ts

--- src/commands/General/Hi.ts
+++ src/commands/General/Hi.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'hi',
+            command: 'ʜɪ',
             description: 'Generally used to check if bot is Up',
             category: 'general',
             usage: `${client.config.prefix}hi`,
