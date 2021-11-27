c5fd55c5a67ad6061d53cc4e6df4ef6c5495e129
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 14:11:08 +0100

    Update Weather.ts

--- src/commands/Educative/Weather.ts
+++ src/commands/Educative/Weather.ts
@@ -9,7 +9,7 @@ import { MessageType } from '@adiwajshing/baileys'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ᴡᴇᴀᴛʜᴇʀ',
+            command: '➣ ᴡᴇᴀᴛʜᴇʀ',
             aliases: ['weather'],
             description: 'Gives you the weather of the given state or city. ',
             category: 'educative',
