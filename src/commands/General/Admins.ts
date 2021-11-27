b53f7237564ae314af71c7490e7d96f9f5bbc996
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 14:32:32 +0100

    Update Admins.ts

--- src/commands/General/Admins.ts
+++ src/commands/General/Admins.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ᴀᴅᴍɪɴs',
+            command: '➣ ᴀᴅᴍɪɴs',
             description: 'Tags all Admins 🎖️',
             category: 'general',
             usage: `${client.config.prefix}admins (Message)`,
