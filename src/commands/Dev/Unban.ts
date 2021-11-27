7e7babd7a2e0550915352796c46dae1d1871ba4c
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 14:00:12 +0100

    Update Unban.ts

--- src/commands/Dev/Unban.ts
+++ src/commands/Dev/Unban.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ᴜɴʙᴀɴ',
+            command: '➣ ᴜɴʙᴀɴ',
             description: 'Unban the tagged users globally',
             category: 'dev',
             usage: `${client.config.prefix}unban [@tag]`,
