19644f275de8c2db456c6e3fc4a3ab3023b295e3
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 13:27:53 +0100

    Update Ethan.ts

--- src/commands/Bots/Ethan.ts
+++ src/commands/Bots/Ethan.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ᴇᴛʜᴀɴ',
+            command: '➣ ᴇᴛʜᴀɴ',
             description: 'Display the info',
             category: 'bots',
             usage: `${client.config.prefix}ethan`,
