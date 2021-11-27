2f5e9083e3a8f1ed45ab864ab44372cf9281032c
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 14:18:09 +0100

    Update Quote.ts

--- src/commands/Fun/Quote.ts
+++ src/commands/Fun/Quote.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ǫᴜᴏᴛᴇ',
+            command: '➣ ǫᴜᴏᴛᴇ',
             description: 'random quote.',
             aliases: ['qu','quote'],
             category: 'fun',
