b27a648d91ef375217747ab36f8ecd2e835ad031
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 01:26:23 +0100

    Update Everyone.ts

--- src/commands/Moderation/Everyone.ts
+++ src/commands/Moderation/Everyone.ts
@@ -8,7 +8,7 @@ export default class Command extends BaseCommand {
         super(client, handler, {
             command: 'ᴇᴠᴇʀʏᴏɴᴇ',
             description: 'Tags all users in group chat',
-            aliases: ['all', 'tagall'],
+            aliases: ['all', 'tagall', 'everyone'],
             category: 'general',
             usage: `${client.config.prefix}everyone`,
             adminOnly: true,
