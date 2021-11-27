788d50cef11e1ba5342b67d31d83d370cea93747
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 12:55:47 +0100

    Update Manga.ts

--- src/commands/Anime/Manga.ts
+++ src/commands/Anime/Manga.ts
@@ -9,7 +9,7 @@ import { MessageType } from '@adiwajshing/baileys'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ᴍᴀɴɢᴀ',
+            command: '➣ ᴍᴀɴɢᴀ',
             description: `Gives you the data of the given manga from MyAnimeList.`,
             aliases: ['mnga', 'manga'],
             category: 'anime',
