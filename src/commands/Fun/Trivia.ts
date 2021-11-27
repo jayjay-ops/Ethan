61c721b8c55e9c702bf8dba0b79dd1485f44ff60
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 14:27:20 +0100

    Update Trivia.ts

--- src/commands/Fun/Trivia.ts
+++ src/commands/Fun/Trivia.ts
@@ -7,7 +7,7 @@ import axios from 'axios'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ᴛʀɪᴠɪᴀ',
+            command: '➣ ᴛʀɪᴠɪᴀ',
             description: 'random trivia.',
             aliases: ['trivia'],
             category: 'fun',
