a2ab00a2dbcefba67baceb8e8685ab7c336018ea
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 15:13:19 +0100

    Update Deactivate.ts

--- src/commands/Moderation/Deactivate.ts
+++ src/commands/Moderation/Deactivate.ts
@@ -7,7 +7,7 @@ export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
             adminOnly: true,
-            command: 'ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇ',
+            command: '➣ ᴅᴇᴀᴄᴛɪᴠᴀᴛᴇ',
             aliases: ['deact', 'deactivate'],
             description: 'deactivate certain features on group-chats',
             category: 'moderation',
