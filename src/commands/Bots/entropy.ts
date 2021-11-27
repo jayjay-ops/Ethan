423c0101eb5a590744b4c4923bda3cd39106ebed
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 13:35:37 +0100

    Update entropy.ts

--- src/commands/Bots/entropy.ts
+++ src/commands/Bots/entropy.ts
@@ -6,7 +6,7 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ᴇɴᴛʀᴏᴘʏ',
+            command: '➣ ᴇɴᴛʀᴏᴘʏ',
             description: 'Displays the info',
             category: 'bots',
             usage: `${client.config.prefix}entropy`,
