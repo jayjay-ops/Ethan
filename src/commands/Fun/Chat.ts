6e83cd969983b935e03bff6c8cc4564a550387df
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-27 14:16:13 +0100

    Update Chat.ts

--- src/commands/Fun/Chat.ts
+++ src/commands/Fun/Chat.ts
@@ -7,7 +7,7 @@ import axios from 'axios'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'ᴄʜᴀᴛ',
+            command: '➣ ᴄʜᴀᴛ',
             description: 'Chat with the Bot in group',
             aliases: ['bot','chat'],
             category: 'fun',
