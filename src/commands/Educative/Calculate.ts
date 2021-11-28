8d799290667508186fb8b31cbd0f496475be5589
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-28 20:00:51 +0100

    Update Calculate.ts

--- src/commands/Educative/Calculate.ts
+++ src/commands/Educative/Calculate.ts
@@ -1,35 +1,30 @@
 /** @format */
 
-import { evaluate } from "mathjs";
-import MessageHandler from "../../Handlers/MessageHandler";
-import BaseCommand from "../../lib/BaseCommand";
-import WAClient from "../../lib/WAClient";
-import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import { evaluate } from 'mathjs'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { IParsedArgs, ISimplifiedMessage } from '../../typings'
 
 export default class Command extends BaseCommand {
-	constructor(client: WAClient, handler: MessageHandler) {
-		super(client, handler, {
-			command: "➣ ᴄᴀʟᴄᴜʟᴀᴛᴇ",
-			aliases: ["calc", 'calculate'],
-			description: "Calculates the given value. ",
-			category: "educative",
-			usage: `${client.config.prefix}calc [value]`,
-			baseXp: 20,
-		});
-	}
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: '➣ ᴄᴀʟᴄᴜʟᴀᴛᴇ',
+            aliases: ['calc', 'calculate'],
+            description: 'Calculates the given value. ',
+            category: 'educative',
+            usage: `${client.config.prefix}calculate [value]`,
+            baseXp: 20
+        })
+    }
 
-	run = async (
-		M: ISimplifiedMessage,
-		{ joined }: IParsedArgs
-	): Promise<void> => {
-		if (!joined)
-			return void M.reply("Provide me the value to calculate, Baka!");
-		const value = joined.trim();
-		// eslint-disable-next-line @typescript-eslint/no-explicit-any
-		const calc = evaluate(value).catch((err: any) => {
-			return void M.reply(`✖️ An error occurred. Reason: *${err}*`);
-		});
-		const text = `💡 *Solution for ${value} = ${calc}*`;
-		await M.reply(text);
-	};
+    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
+        if (!joined) return void M.reply('Provide the value to calculate, Baka!')
+        const value = joined.trim()
+        const calc = evaluate(value);
+				const text = `💡 *Solution for ${value} = ${calc}*`;
+        await M.reply(text)
+            // eslint-disable-next-line @typescript-eslint/no-explicit-any
+            .catch((reason: any) => M.reply(`${reason}`))
+    }
 }
