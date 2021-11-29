7c0d4f36215e2f447bf8bbda168911bd0f72285b
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-11-29 13:57:49 +0100

    Create Truth.ts

---
+++ src/commands/Games/Truth.ts
@@ -0,0 +1,26 @@
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import request from '../../lib/request'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+const r = require('better-tord');
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: '➣ ᴛʀᴜᴛʜ',
+            description: 'Gives you random questions',
+            category: 'games',
+            aliases: ['truth','tr']
+            usage: `${client.config.prefix}truth`,
+            
+        })
+    }
+
+    
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+const dare = r.get_truth();
+		await M.reply(dare);
+	};
+}
