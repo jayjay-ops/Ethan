72ebcd6229455cc29fcd41ae09cf789453490252
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-01 09:44:40 +0100

    Update Ethan.ts

--- src/commands/Bots/Ethan.ts
+++ src/commands/Bots/Ethan.ts
@@ -48,6 +48,6 @@ export default class Command extends BaseCommand {
             `*❗🄴🅃🄷🄰🄽-🄱🄾🅃*\n\n *📮【About】:* 𝑨𝒖𝒕𝒐𝒎𝒂𝒕𝒆𝒅 𝑯𝒆𝒍𝒑𝒇𝒖𝒍/𝑭𝒖𝒏 𝑩𝒐𝒕 𝒕𝒐 𝒎𝒂𝒌𝒆 𝒍𝒊𝒇𝒆 𝒆𝒂𝒔𝒊𝒆𝒓 𝒐𝒏 𝑾𝒉𝒂𝒕𝒔𝑨𝒑𝒑\n\n*🏅【Support】:* https://www.instagram.com/jayjay_ops\n\n*🛸【Group】:* https://chat.whatsapp.com/EBUKlcbfc3692tllAcEAP7 \n`,
             MessageType.text
         )
-        return void M.reply('Sent you the support info in personal message').catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
+        return void M.reply('Sent you the support info in personal message'); //.catch((reason: Error) => M.reply(`an error occurred, Reason: ${reason}`))
     }
 }
