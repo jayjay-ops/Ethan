0e5230609bfad805707a380b404d85a4d934d5d5
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-01 15:54:13 +0100

    Update jail.ts

--- src/commands/Fun/jail.ts
+++ src/commands/Fun/jail.ts
@@ -1,36 +1,41 @@
-import { MessageType } from '@adiwajshing/baileys'
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import WAClient from '../../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from '../../typings'
-import axios from 'axios'
+/** @format */
+
+import Canvacord from "canvacord";
+import { MessageType } from "@adiwajshing/baileys";
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: '➣ ᴊᴀɪʟ',
-            description: 'to send people to jail who are horny',
-            category: 'fun',
-            usage: `${client.config.prefix}jail [(as caption | quote)[image] | @mention]`,
-            aliases: ['jail'],
-            baseXp: 30
-        })
-    }
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "➣ ᴊᴀɪʟ",
+			description: "Who wanna go to jail for being horny?",
+			category: "fun",
+                        aliases: ['jail']
+			usage: `${client.config.prefix}jail [tag/quote]`,
+			baseXp: 20,
+		});
+	}
 
-    run = async (M: ISimplifiedMessage, { joined }: IParsedArgs): Promise<void> => {
-        const image = await (M.WAMessage?.message?.imageMessage
-            ? this.client.downloadMediaMessage(M.WAMessage)
-            : M.quoted?.message?.message?.imageMessage
-            ? this.client.downloadMediaMessage(M.quoted.message)
-            : M.mentioned[0]
-            ? this.client.getProfilePicture(M.mentioned[0])
-            : this.client.getProfilePicture(M.quoted?.sender || M.sender.jid))
-
-            await axios.get(`https://api.dhamzxploit.my.id/api/canvas/wanted?url=${image}`)
-            .then((response)=>{
-                M.reply(response.data)
-            }).catch((e)=>{
-                M.reply('sorry couldn\'t send the image')
-            })
-
-    }
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		const image = await (M.WAMessage?.message?.imageMessage
+			? this.client.downloadMediaMessage(M.WAMessage)
+			: M.quoted?.message?.message?.imageMessage
+			? this.client.downloadMediaMessage(M.quoted.message)
+			: M.mentioned[0]
+			? this.client.getProfilePicture(M.mentioned[0])
+			: this.client.getProfilePicture(M.quoted?.sender || M.sender.jid));
+		if (!image) return void M.reply(`Couldn't fetch the required Image`);
+		const result = await Canvacord.Canvacord.jail(image, false);
+		await M.reply(
+			result,
+			MessageType.image,
+			undefined,
+			undefined,
+			undefined,
+			undefined
+		);
+	};
 }
