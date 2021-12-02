3358d9628cb49e4b40eb3f2963f9361ca5167b49
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-02 16:32:16 +0100

    Update Rank.ts

--- src/commands/General/Rank.ts
+++ src/commands/General/Rank.ts
@@ -86,37 +86,46 @@ export default class Command extends BaseCommand {
 
 
 
-import { MessageType } from '@adiwajshing/baileys'
-import MessageHandler from '../../Handlers/MessageHandler'
-import BaseCommand from '../../lib/BaseCommand'
-import request from '../../lib/request'
-import WAClient from '../../lib/WAClient'
-import { IParsedArgs, ISimplifiedMessage } from "../../typings";
-const yuricanvas = require("yuri-canvas");
+
+import MessageHandler from "../../Handlers/MessageHandler";
+import BaseCommand from "../../lib/BaseCommand";
+import WAClient from "../../lib/WAClient";
+import { ISimplifiedMessage } from "../../typings";
+import Canvacord from "canvacord";
+import { MessageType } from "@adiwajshing/baileys";
 
 export default class Command extends BaseCommand {
-    constructor(client: WAClient, handler: MessageHandler) {
-        super(client, handler, {
-            command: 'rank',
-            description: 'Displays user-rank 📜',
-            category: 'general',
-            usage: `${client.config.prefix}rank [tag/quote]`,
-            aliases: ['rk', 'rnk'],
-            baseXp: 20
-        })
-    }
+	constructor(client: WAClient, handler: MessageHandler) {
+		super(client, handler, {
+			command: "➣ ʀᴀɴᴋ",
+			description: "Displays User's Stats",
+			category: "general",
+			usage: `${client.config.prefix}rank [tag/quote]`,
+			aliases: ["rank","stats"],
+			baseXp: 10,
+		});
+	}
 
-run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => { 
-      if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-        const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
-        let username = user === M.sender.jid ? M.sender.username : ''
-        if (!username) {
-            const contact = this.client.getContact(user)
-            username = contact.notify || contact.vname || contact.name || user.split('@')[0]
-        }
-     const exp = (await this.client.getUser(user)).Xp || 0 
-        let role
-        if (exp < 500) {
+	run = async (M: ISimplifiedMessage): Promise<void> => {
+		if (M.quoted?.sender) M.mentioned.push(M.quoted.sender);
+		const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid;
+		let username = user === M.sender.jid ? M.sender.username : "";
+		if (!username) {
+			const contact = this.client.getContact(user);
+			username =
+				contact.notify || contact.vname || contact.name || user.split("@")[0];
+		}
+		let pfp: string;
+		try {
+			pfp = await this.client.getProfilePicture(user);
+		} catch (err) {
+			M.reply(`Profile Picture not Accessible of ${username}`);
+			pfp =
+				"https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg";
+		}
+		const exp = (await this.client.getUser(user)).Xp;
+		let role;
+		if (exp < 500) {
 			role = "🤽 Citizen";
 		} else if (exp < 1000) {
 			role = "🧝 Cleric";
@@ -137,83 +146,75 @@ run = async (M: ISimplifiedMessage, parsedArgs: IParsedArgs): Promise<void> => {
 		} else {
 			role = "🗽 Mystic";
 		}
-
-        let level
-        if (exp < 500) {
-            level = '1'
-        } else if (exp < 1000) {
-            level = '2'
-        } else if (exp < 2000) {
-            level = '3'
-        } else if (exp < 5000) {
-            level = '4'
-        } else if (exp < 10000) {
-            level = '5'
-        } else if (exp < 25000) {
-            level = '6'
-        } else if (exp < 50000) {
-            level = '7'
-        } else if (exp < 75000) {
-            level = '8'
-        } else if (exp < 100000) {
-            level = '9'
-        } else {
-            level = 'Max'
-        }
-        
-        let rxp = 10
-        if (exp < 500) {
-            level = '1000'
-        } else if (exp < 1000) {
-            level = '2000'
-        } else if (exp < 2000) {
-            level = '3000'
-        } else if (exp < 5000) {
-            level = '10000'
-        } else if (exp < 10000) {
-            level = '20000'
-        } else if (exp < 25000) {
-            level = '30000'
-        } else if (exp < 50000) {
-            level = '60000'
-        } else if (exp < 75000) {
-            level = '80000'
-        } else if (exp < 100000) {
-            level = 'Max'
-        } else {
-            level = 'Max × Infinity'
-        }
-        
-let pfp: string
-        try {
-            pfp = await this.client.getProfilePicture(user)
-        } catch (err) {
-            M.reply(`Profile Picture not Accessible of ${username}`)
-            pfp =
-                'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRWhshgkXh025auPy0RDEeY9j3B14jlZMqgaw&usqp=CAU'
-        }
-let discrim = `${rxp}`
-let image = await yuricanvas.rank({ 
-            username,  
-            discrim,
-            level: rxp, 
-            rank: role, 
-            totalXP: level,
-            neededXP: level, 
-            currentXP: exp || 0, 
-            avatarURL: pfp, 
-            color: "black", 
-            background: "https://upload.wikimedia.org/wikipedia/commons/7/71/White.png"
-        });
-M.reply(image,
-        MessageType.image,
-                    undefined,
-                    undefined,
-                    `🏮 *Username: ${username}*\n\n*${username}'s Rank:*\n\n🎀 *Level: ${level}*\n\n🌟 *Exp: ${
+		// eslint-disable-next-line @typescript-eslint/no-explicit-any
+		let level: number;
+		if (exp < 500) {
+			level = 1;
+		} else if (exp < 1000) {
+			level = 2;
+		} else if (exp < 2000) {
+			level = 3;
+		} else if (exp < 5000) {
+			level = 4;
+		} else if (exp < 10000) {
+			level = 5;
+		} else if (exp < 25000) {
+			level = 6;
+		} else if (exp < 50000) {
+			level = 7;
+		} else if (exp < 75000) {
+			level = 8;
+		} else if (exp < 100000) {
+			level = 9;
+		} else {
+			level = 10;
+		}
+		let required: number;
+		if (exp < 500) {
+			required = 500;
+		} else if (exp < 1000) {
+			required = 1000;
+		} else if (exp < 2000) {
+			required = 2000;
+		} else if (exp < 5000) {
+			required = 5000;
+		} else if (exp < 10000) {
+			required = 10000;
+		} else if (exp < 25000) {
+			required = 25000;
+		} else if (exp < 50000) {
+			required = 50000;
+		} else if (exp < 75000) {
+			required = 75000;
+		} else if (exp < 100000) {
+			required = 100000;
+		} else {
+			required = 0;
+		}
+		const rank = new Canvacord.Rank()
+			.setAvatar(pfp)
+			.setCurrentXP(exp || 0)
+			.setRequiredXP(required)
+			.setStatus("online", true)
+			.setLevel(level, "Level:", true)
+			.setRank(0, `Role: ${role}`, true)
+			.setProgressBar("#e68e15", "COLOR")
+			.setOverlay("#000000")
+			.setUsername(username)
+			.setDiscriminator("0001")
+			.setBackground("COLOR", "#ffffff");
+		rank.build({}).then((rankcard) => {
+			const text = `*${username}'s Rank:*\n\n🎀 *Level: ${level}*\n\n🌟 *Exp: ${
 				exp || 0
-			}*\n\n♻️ *Role: ${role}* `,
-                    undefined
-                ).catch((reason: any) =>
-            M.reply(`✖ An error occurred. Please try again later. ${reason}`))
-    }
+			}*\n\n♻️ *Role: ${role}*`;
+			M.reply(
+				rankcard,
+				MessageType.image,
+				undefined,
+				undefined,
+				text,
+				undefined
+			);
+		});
+	};
 }
