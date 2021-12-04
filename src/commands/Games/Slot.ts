b6bcbebd17f63b6fe01c032646c92ac5d461613e
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-04 17:49:57 +0100

    Update Slot.ts

--- src/commands/Games/Slot.ts
+++ src/commands/Games/Slot.ts
@@ -19,14 +19,13 @@ export default class Command extends BaseCommand {
     run = async (M: ISimplifiedMessage): Promise<void> => {
          const slot = readFileSync(this.client.assets.get('slot'));
          const exp = (await this.client.getUser(user)).Xp;
-         //const userxpslot = exp[0]
-         //let oldxp = userxpslot;
-        if (${exp} < 1000)
-           M.reply("not enough experince to play")
+   
+         if (${exp} < 1000)
+            M.reply("not enough experince to play")
+
 }
 
 
 
-//if (xp < 1000)
 
 
