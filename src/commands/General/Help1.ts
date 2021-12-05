c63454a2d23f58ac342f26e6c5a48ed86206f124
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-05 13:29:12 +0100

    Update Help1.ts

--- src/commands/General/Help1.ts
+++ src/commands/General/Help1.ts
@@ -9,12 +9,12 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: 'help1',
+            command: '➩ ʜᴇʟᴘ1',
             description: 'Displays the help menu or shows the info of the command provided',
             category: 'general',
             usage: `${client.config.prefix}help1 (command_name)`,
             dm: true,
-            aliases: ['h1']
+            aliases: ['help1','h1']
         })
     }
 
@@ -25,22 +25,22 @@ export default class Command extends BaseCommand {
         let ethan = n[Math.floor(Math.random() * n.length)]
         return void this.client.sendMessage(M.from, { url: ethan }, MessageType.video, {quoted:M.WAMessage,
             mimetype: Mimetype.gif,
-            caption: `-Ⓖ︎Ⓔ︎Ⓝ︎Ⓔ︎Ⓡ︎Ⓐ︎Ⓛ︎ ⛲
+            caption: `-Ⓖ︎Ⓔ︎Ⓝ︎Ⓔ︎Ⓡ︎Ⓐ︎Ⓛ︎ 🎐
 ──────────────
-🧧 ${this.client.config.prefix}ᴡᴀʟʟᴘᴀᴘᴇʀ
-🧧 ${this.client.config.prefix}xᴘ
-🧧 ${this.client.config.prefix}ʀᴀɴᴋ
-🧧 ${this.client.config.prefix}ᴘʀᴏғɪʟᴇ
-🧧 ${this.client.config.prefix}ᴍᴏᴅs/ᴏᴡɴᴇʀ
-🧧 ${this.client.config.prefix}ɪɴᴠɪᴛᴇʟɪɴᴋ
-🧧 ${this.client.config.prefix}ʜᴇʟᴘ
-🧧 ${this.client.config.prefix}ʜɪ
-🧧 ${this.client.config.prefix}ᴅᴇʟᴇᴛᴇ
-🧧 ${this.client.config.prefix}ɢɪᴛʜᴜʙ
-🧧 ${this.client.config.prefix}ɢᴏᴏɢʟᴇ
-🧧 ${this.client.config.prefix}ᴛʀᴀɴsʟᴀᴛᴇ
-🧧 ${this.client.config.prefix}ᴡɪᴋɪᴘᴇᴅɪᴀ
-🧧 ${this.client.config.prefix}ɪɴғᴏ
+⛲ ${this.client.config.prefix}ᴡᴀʟʟᴘᴀᴘᴇʀ
+⛲ ${this.client.config.prefix}xᴘ
+⛲ ${this.client.config.prefix}ʀᴀɴᴋ
+⛲ ${this.client.config.prefix}ᴘʀᴏғɪʟᴇ
+⛲ ${this.client.config.prefix}ᴍᴏᴅs/ᴏᴡɴᴇʀ
+⛲ ${this.client.config.prefix}ɪɴᴠɪᴛᴇʟɪɴᴋ
+⛲ ${this.client.config.prefix}ʜᴇʟᴘ
+⛲ ${this.client.config.prefix}ʜɪ
+⛲ ${this.client.config.prefix}ᴅᴇʟᴇᴛᴇ
+⛲ ${this.client.config.prefix}ɢɪᴛʜᴜʙ
+⛲ ${this.client.config.prefix}ɢᴏᴏɢʟᴇ
+⛲ ${this.client.config.prefix}ᴛʀᴀɴsʟᴀᴛᴇ
+⛲ ${this.client.config.prefix}ᴡɪᴋɪᴘᴇᴅɪᴀ
+⛲ ${this.client.config.prefix}ɪɴғᴏ
 
 ──────────────` }
         )
