9bb0c6d38240fdefbb2896b7b1c3e161feed4870
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-09 00:41:06 +0100

    Update Dare.ts

--- src/commands/Games/Dare.ts
+++ src/commands/Games/Dare.ts
@@ -14,7 +14,7 @@ export default class Command extends BaseCommand {
             category: 'games',
             aliases: ['dare', 'dr'],
             usage: `${client.config.prefix}dare`,
-            baseCoin: 30
+            baseCoin: 5
             
         })
     }
