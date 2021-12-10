2b0552a602926d1d0b208c39755aecb9aa20f3d7
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-10 13:52:31 +0100

    Create Report.ts

---
+++ src/commands/General/Report.ts
@@ -0,0 +1,24 @@
+import { MessageType } from '@adiwajshing/baileys'
+import MessageHandler from '../../Handlers/MessageHandler'
+import BaseCommand from '../../lib/BaseCommand'
+import WAClient from '../../lib/WAClient'
+import { ISimplifiedMessage } from '../../typings'
+
+export default class Command extends BaseCommand {
+    constructor(client: WAClient, handler: MessageHandler) {
+        super(client, handler, {
+            command: '➣ ʀᴇᴘᴏʀᴛ',
+            description: 'reports a bug to the own',
+            category: 'bots',
+            usage: `${client.config.prefix}ethan`,
+            aliases: ['report'],
+            baseXp: 10
+        })
+    }
+
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+       const msg = ""
+const mods = this.client.config.mods
+for(const numbers of mods){
+this.client.sendMessage(numbers,msg,MessageType.text)} 
+}
