7df6b8abda16bed8b90736b692d65cb062def75b
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-10 01:29:52 +0100

    Update Wallet.ts

--- src/commands/General/Wallet.ts
+++ src/commands/General/Wallet.ts
@@ -24,6 +24,6 @@ export default class Command extends BaseCommand {
             // username = contact.notify || contact.vname || contact.name || user.split('@')[0]
             username = user.split('@')[0]
         }
-        return void (await M.reply(`*👛 ${username}'s wallet*\n\n🪙 ᑕOIᑎ ➪ *_${(await this.client.getUser(user)).Coin || 0}\n🌟 ᙭ᑭ ➪ *_${(await this.client.getUser(user)).Xp || 0}`
+        return void (await M.reply(`*👛 ${username}'s wallet*\n\n🪙 *ᑕOIᑎ ➪* ${(await this.client.getUser(user)).Coin || 0}\n🌟 *᙭ᑭ ➪* ${(await this.client.getUser(user)).Xp || 0}`
     }
 }
