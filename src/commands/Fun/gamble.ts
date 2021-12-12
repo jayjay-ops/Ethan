d5d94a84be6c535585f3332a064c6251c9246079
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-12 17:30:14 +0100

    Update gamble.ts

--- src/commands/Fun/gamble.ts
+++ src/commands/Fun/gamble.ts
@@ -28,7 +28,7 @@ const texts = joined.trim().split("|");
 		const opp = texts[0]; //your opinion 
 		const value = texts[1];// your value
                 const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
-                const coin = (await this.client.getUser(user)).Coin
+                const coin = (await this.client.getUser(user)).Xp
 		const bank = 5000
 		if (!value) return void M.reply("Give me the value, Baka!");
 		if (!opp) return void M.reply("Give me the direction, Baka!");
