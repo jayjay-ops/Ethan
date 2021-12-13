fdd70016789c917c5ee219e977dbd9264efc62be
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-13 23:15:49 +0100

    Update daily.ts

--- src/commands/Fun/daily.ts
+++ src/commands/Fun/daily.ts
@@ -4,16 +4,15 @@ import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
-import axios from 'axios'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
 
-       if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
-            const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
-            let coin = 1000
-            await this.client.setCoin(user,coin);
-                M.reply("1000 has been added to your account")
+            if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+        const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
+        let coin = 1000
+        await this.client.setCoin(user,coin);
+              M.reply("1000 has been added to your account")
 }
 }
