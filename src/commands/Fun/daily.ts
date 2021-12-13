e89c9faba759a81c4ba6a5f8131219b69f811509
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-13 23:23:45 +0100

    Update daily.ts

--- src/commands/Fun/daily.ts
+++ src/commands/Fun/daily.ts
@@ -1,5 +1,3 @@
-import { MessageType } from '@adiwajshing/baileys'
-import request from '../../lib/request'
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
@@ -8,11 +6,21 @@ import { ISimplifiedMessage } from '../../typings'
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
+            command: '➣ daily',
+            description: "claim daily cojn 🌟",
+            category: 'fun',
+            usage: `${client.config.prefix}daily`,
+            aliases: ['daily', 'claim'],
+            baseXp: 10
+        })
+    }
 
-            if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+    run = async (M: ISimplifiedMessage): Promise<void> => {
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
         let coin = 1000
         await this.client.setCoin(user,coin);
               M.reply("1000 has been added to your account")
-}
+        
+    }
 }
