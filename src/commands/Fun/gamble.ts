e4322ea11c1c5944f313adc7abc779043abd8d05
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-28 01:02:39 +0100

    Update gamble.ts

--- src/commands/Fun/gamble.ts
+++ src/commands/Fun/gamble.ts
@@ -2,6 +2,12 @@ import MessageHandler from "../../Handlers/MessageHandler";
 import BaseCommand from "../../lib/BaseCommand";
 import WAClient from "../../lib/WAClient";
 import { IParsedArgs, ISimplifiedMessage } from "../../typings";
+import { MessageType, Mimetype } from '@adiwajshing/baileys'
+import { Sticker, Categories, StickerTypes } from 'wa-sticker-formatter'
+import axios from 'axios'
+const eco = require('discord-mongoose-economy');
+const ty = eco.connect('mongodb+srv://jayjay-ops:jayjay-ops@cluster0.orjsg.mongodb.net/test');
+
 
 export default class Command extends BaseCommand {
 	constructor(client: WAClient, handler: MessageHandler) {
@@ -10,7 +16,7 @@ export default class Command extends BaseCommand {
 			aliases: ["gr"],
 			description: "You can gamble. ",
 			category: "fun",
-			usage: `${client.config.prefix}gamble <value>|<direction>\n\nExample: ${client.config.prefix}gamble 100|right`,
+			usage: `${client.config.prefix}gamble <value> <direction>\n\nExample: ${client.config.prefix}gamble 100 right`,
 			baseXp: 40,
 		});
 	}
@@ -20,35 +26,67 @@ export default class Command extends BaseCommand {
 		{ joined }: IParsedArgs
 	): Promise<void> => {
 
-const texts = joined.trim().split("|");
+const texts = joined.trim().split(" ");
 		if (texts[0] === "")
 			return void M.reply(
-				`Use ${this.client.config.prefix}gamble (100|right)`
+				`Use ${this.client.config.prefix}gamble (100 right)`
 			);
-		const opp = texts[0]; //your opinion 
-		const value = texts[1];// your value
-                const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
-                const coin = (await this.client.getUser(user)).Xp
-		const bank = 5000
-		if (!value) return void M.reply("Give me the value, Baka!");
+		const opp = texts[1];// your value
+		const value = texts[0].toLowerCase();
+if (!value) return void M.reply("Give me the value, Baka!");
 		if (!opp) return void M.reply("Give me the direction, Baka!");
-		var typ = [
-			"right",
-			"left",
-			"left",
-			"right",
-		];
-const random = typ[Math.floor(Math.random() * typ.length)];
-let a = (bank) > (value || 0)
-if ( a === true) {
-  if ( opp == random ) {
-  	await M.reply(`You won ${value}`)
-} else {
-await M.reply(`you lost ${value}`)
+let gg = parseInt(value)
+if (isNaN(gg)) return void M.reply("check your text plz u r using the command in a wrong way")
 
-}
-} else {
- await M.reply("sorry, you dont have sufficient coins to gamble")
+        if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
+        const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
+		const cara = 'cara'
+const balance = await eco.balance(user, cara); 
+let g = (balance.wallet) > parseInt(value)
+if(g == false) return void M.reply("you cant baka check your wallet ");
+let k = 49
+let a = (k) > parseInt(value)
+//Returns wallet, bank, and bankCapacity. Also creates a USer if it doesn't exist.	
+if(a == true) return void M.reply("you cant baka do with more than 50");
+                //if(balance.wallet < value) return void M.reply('no enough money');
+const F = ["left", "right"]
+const r = F[Math.floor(Math.random () * F.length)]
+switch (r) {
+  case 'right':
+  let right = "https://www.freeiconspng.com/thumbs/arrow-icon/right-arrow-icon-27.png"
+  	let stcker: any = await new Sticker(right, {
+			pack: "sticker",
+			author: "BY cara",
+			quality: 90,
+			type: "crop",
+			categories: ["🎊"],
+		});
+await M.reply(
+			await stcker.build(),
+			MessageType.sticker,
+			Mimetype.webp,)
+    break
+  case 'left':
+  let left = "https://www.pngarts.com/files/2/Left-Arrow-PNG-Download-Image.png"
+  	let stiker: any = await new Sticker(left, {
+			pack: "sticker",
+			author: "BY cara",
+			quality: 90,
+			type: "crop",
+			categories: ["🎊"],
+		});
+await M.reply(
+			await stiker.build(),
+			MessageType.sticker,
+			Mimetype.webp,)
+    break
+};
+if ( r == opp){
+const give = await eco.give(user, cara, value);
+M.reply(`📈 *You won ${value}*`)
+}else{
+const deduct = await eco.deduct(user, cara, value);
+M.reply(`📉 *you lost ${value}*`)
 }
 }
 }
