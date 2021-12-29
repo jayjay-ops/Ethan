6e14c085fad9673fc9261cfd0697db05784e930b
JayJay Ops <93006483+jayjay-ops@users.noreply.github.com>
2021-12-29 20:32:59 +0100

    Update Wallet.ts

--- src/commands/General/Wallet.ts
+++ src/commands/General/Wallet.ts
@@ -1,22 +1,24 @@
-
+const eco = require('discord-mongoose-economy');
+const ty = eco.connect('');
 import MessageHandler from '../../Handlers/MessageHandler'
 import BaseCommand from '../../lib/BaseCommand'
 import WAClient from '../../lib/WAClient'
 import { ISimplifiedMessage } from '../../typings'
+import request from '../../lib/request'
 
 export default class Command extends BaseCommand {
     constructor(client: WAClient, handler: MessageHandler) {
         super(client, handler, {
-            command: '➣ ᴡᴀʟʟᴇᴛ',
+            command: 'wallet',
             description: "Displays User's wallet 💰",
-            category: 'general',
+            category: 'fun',
             usage: `${client.config.prefix}wallet (@tag)`,
             aliases: ['bank', 'wallet'],
             baseXp: 10
         })
     }
 
-    run = async (M: ISimplifiedMessage): Promise<void> => {
+run = async (M: ISimplifiedMessage): Promise<void> => {
         if (M.quoted?.sender) M.mentioned.push(M.quoted.sender)
         const user = M.mentioned[0] ? M.mentioned[0] : M.sender.jid
         let username = user === M.sender.jid ? M.sender.username : '🤷🏽‍♂️'
@@ -25,7 +27,9 @@ export default class Command extends BaseCommand {
             // username = contact.notify || contact.vname || contact.name || user.split('@')[0]
             username = user.split('@')[0]
         }
-        return void (await M.reply(`*👛 ${username}'s wallet*\n\n🌟𝐗𝐩 ➪ *_${(await this.client.getUser(user)).Xp || 0}_*\n🪙 Coins  ➪ ${(await this.client.getUser(user)).Coin || 0}*`))
-    }
-}
+const cara = "cara"
+const balance = await eco.balance(user, cara); //Returns wallet, bank, and bankCapacity. Also creates a USer if it doesn't exist.
 
+await M.reply(`👛 Wallet: ⛀${balance.wallet}\n\n 🏦 Bank: ⛁${balance.bank}/${balance.bankCapacity}`);
+}
+}
